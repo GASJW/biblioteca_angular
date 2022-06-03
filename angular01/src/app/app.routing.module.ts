a72d453dad7de6a8f089faf98330dc35fe76ccf3
Guilherme Augusto da Silva <44906357+GASJW@users.noreply.github.com>
2022-06-03 11:03:46 -0300

    main

--- angular01/src/app/app.routing.module.ts
+++ angular01/src/app/app.routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 
 const routes: Routes = [
   {
@@ -20,8 +22,12 @@ const routes: Routes = [
       import('./components/menu/menu.module').then((me) => me.MenuModule),
   },
   {
+    path: '',
+    component: HomeComponent,
+  },
+  {
     path: '**',
-    redirectTo: 'livros',
+    component: PagenotfoundComponent,
   },
 ];
 
---
+++ angular01/src/app/components/home/home.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './home.component';
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [],
+})
+export class HomeModule {}
